548e113ec671e56d3cc1b6a709b763b2d864377d
Vipin Nair <swvist@gmail.com>
2012-03-29 23:46:19 +0530

    Links different metrics of a team together. Sample data of 'teammetrics' project is included.

---
+++ application/teamlist.py
@@ -0,0 +1,16 @@
+"""
+
+This file interlinks teams to different metrics.
+Sample data only links the 'teammetrics' data together.
+
+"""
+
+teams	=	{
+					"teammetrics"	:	{
+											"authorstats"	:	"teammetrics-discuss",
+											"commitstats"	:	"teammetrics",
+											"commitlines"	:	"teammetrics",
+											"bugs"			:	None,
+											"uploaders"		:	None
+										}
+			}
