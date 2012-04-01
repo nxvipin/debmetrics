aead49218062ffd3d495f885981865d900850d19
Vipin Nair <swvist@gmail.com>
2012-04-01 15:40:59 +0530

    Added views to render static and dynamic charts.

--- application/views.py
+++ application/views.py
@@ -6,11 +6,20 @@ URL route handlers
 """
 
 from flask import render_template, flash, url_for, redirect, json
-from flask import jsonify
+from flask import render_template
 
 
 def index():
 	return "Test Flask App"
 
-def teamMetrics(team, metric):
-	return "<img src=http://blends.debian.net/liststats/"+metric+"_"+team+".png>"
+def teamMetricsStaticImage(team, metric):
+	d=dict();
+	d["team"]=team;
+	d["metric"]=metric;
+	return render_template('chart.html',data=d)
+
+def teamMetricsDynamicImage(team, metric):
+	d=dict();
+	d["team"]=team;
+	d["metric"]=metric;
+	return render_template('dchart.html',data=d)
